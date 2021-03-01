10f33542faf4a6b8c07e845c00ad322dd39e140e
Yuri Trondin <lopata@2ch.hk>
2021-03-01 14:16:24 +0500

    верстка страницы логина

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Authentication from "../components/Topbar/Authentication/Authentication";
 import Article from "./article/article";
 import GlobalFeed from "./globalFeed/globalFeed";
 
@@ -7,6 +8,9 @@ import GlobalFeed from "./globalFeed/globalFeed";
   return (
     <Switch>
       <Route exact path="/" component={GlobalFeed} />
+      <Route  path="/login" component={Authentication} />
+      <Route  path="/register" component={Authentication} />
+      <Route  path="/" component={GlobalFeed} />
       <Route path="/arcticles/:slug" component={Article} />
     </Switch>
   );
